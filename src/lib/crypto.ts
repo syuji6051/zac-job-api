4566a9c353d84e81b4eb7d588819fac710a5844c
syuji <syuji.okamura@gmail.com>
2020-12-23 13:04:58 +0900

    commit

--- src/lib/crypto.ts
+++ src/lib/crypto.ts
@@ -2,6 +2,7 @@ import crypto from 'crypto';
 
 const ALGORITHM = 'aes-256-cbc';
 const ENCODING = 'base64';
+const iv = 'a2xhcgAAAAAAAAAA';
 
 // eslint-disable-next-line import/prefer-default-export
 export function encrypt(encryptKey: string, encryptSaltKey: string, message: string) {
@@ -9,10 +10,10 @@ export function encrypt(encryptKey: string, encryptSaltKey: string, message: str
     throw new Error('ENCRYPT_KEY or ENCRYPT_SALT_KEY is not found');
   }
   const key = crypto.scryptSync(encryptKey, encryptSaltKey, 32);
-  const iv = crypto.randomBytes(16);
+  // const iv = crypto.randomBytes(16);
   const cipher = crypto.createCipheriv(ALGORITHM, key, iv);
   const encryptedData = cipher.update(Buffer.from(message));
-  return Buffer.concat([iv, encryptedData, cipher.final()]).toString(ENCODING);
+  return Buffer.concat([encryptedData, cipher.final()]).toString(ENCODING);
 }
 
 export function decrypt(encryptKey: string, encryptSaltKey: string, encrypted: string) {
@@ -21,9 +22,9 @@ export function decrypt(encryptKey: string, encryptSaltKey: string, encrypted: s
   }
   const key = crypto.scryptSync(encryptKey, encryptSaltKey, 32);
   const buff = Buffer.from(encrypted, 'base64');
-  const iv = buff.slice(0, 16);
-  const encData = buff.slice(16);
+  // const iv = buff.slice(0, 16);
+  // const encData = buff.slice(16);
   const decipher = crypto.createDecipheriv(ALGORITHM, key, iv);
-  const decryptData = decipher.update(encData);
-  return Buffer.concat([decryptData, decipher.final()]).toString('utf8');
+  const decryptData = decipher.update(buff);
+  return Buffer.concat([decryptData, decipher.final()]);
 }
