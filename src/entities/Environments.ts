23a37187b79644760909d6d41f9866b71162a31b
岡村修吏 <syuji.okamura@gmail.com>
2021-06-12 16:35:34 +0900

    Delete Environments.ts

--- src/entities/Environments.ts
+++
@@ -1,8 +0,0 @@
-export type SecretsValues = {
-  COGNITO_USER_POOL: string
-  ENCRYPT_KEY: string
-  ENCRYPT_SALT_KEY: string
-  PUPPETEER_API_URL: string
-  PUPPETEER_API_ACCESS_KEY: string
-  PUPPETEER_API_SECRET_KEY: string
-};
