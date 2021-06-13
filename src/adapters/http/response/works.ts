d20c27d9b72dc80163e9dcab8f988ee30923870d
syuji.okamura <syuji.okamura@gmail.com>
2021-06-13 23:41:40 +0900

    day utc

--- src/adapters/http/response/works.ts
+++ src/adapters/http/response/works.ts
@@ -1,17 +1,24 @@
+import dayjs from 'dayjs';
+import timezone from 'dayjs/plugin/timezone';
+import utc from 'dayjs/plugin/utc';
 import { APIGatewayProxyResult } from 'aws-lambda';
 import { views } from '@syuji6051/zac-job-library';
+
 import {
   WorkListOutput as IWorkListOutput,
   WorkClockVoidOutput as IWorkClockVoidOutput,
 } from '@/src/usecases/outputs/works';
 import { WorkRecord } from '@/src/entities/dynamodb/works';
 
+dayjs.extend(timezone);
+dayjs.extend(utc);
+dayjs.tz.setDefault('Asia/Tokyo');
 export class WorkListOutput implements IWorkListOutput {
   public success(works: WorkRecord[]): APIGatewayProxyResult {
     return views.success(works
       .filter((work) => work.clockIn || work.clockOut)
       .map((work) => ({
-        day: work.day,
+        day: dayjs.utc(work.day).local().format('YYYY-MM-DD'),
         clock_in: work.clockIn,
         clock_out: work.clockOut,
         go_out_1: work.goOut1,
