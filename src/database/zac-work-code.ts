66d0f982d4b290795cfd140eaab5223607c5c67c
syuji <syuji.okamura@gmail.com>
2021-07-25 05:08:34 +0900

    not record error

--- src/database/zac-work-code.ts
+++ src/database/zac-work-code.ts
@@ -1,15 +1,23 @@
 import mapper from '@/src/database/dynamo-mapper';
 import { ZacWorkCodeRecord } from '@/src/entities/dynamodb/zac-work-code';
 import { WorkCode } from '@/src/entities/zac';
+import { ItemNotFoundException } from '@aws/dynamodb-data-mapper';
 
 // eslint-disable-next-line import/prefer-default-export
 export class ZacWorkCode {
   public async get(userId: string, yearMonth: number): Promise<WorkCode[]> {
     // eslint-disable-next-line new-parens
-    const { codeList } = await mapper.get(Object.assign(new ZacWorkCodeRecord, {
-      userId, yearMonth,
-    }));
-    return codeList;
+    try {
+      const { codeList } = await mapper.get(Object.assign(new ZacWorkCodeRecord(), {
+        userId, yearMonth,
+      }));
+      return codeList;
+    } catch (err) {
+      if (err.name === ItemNotFoundException.name) {
+        return [];
+      }
+      throw err;
+    }
   }
 
   public async set(userId: string, yearMonth: number, workCodeList: WorkCode []) {
